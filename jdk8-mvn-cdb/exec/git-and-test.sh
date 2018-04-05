161cf66c7005f37159cb37706c8d28fa895ec5e0
alejeau <alejeau@excilys.com>
2018-04-05 11:57:31 +0200

    [FEAT] git-and-test.sh now pulls if the projects exists, clones otherwise.

--- jdk8-mvn-cdb/exec/git-and-test.sh
+++ jdk8-mvn-cdb/exec/git-and-test.sh
@@ -9,16 +9,22 @@ CONFIG_FILE=properties/db.properties
 IP_ADDRESS=172.18.0.2
 ADDRESS_PORT=3306
 
-# Remove the old GIT_TARGET
-rm -rf $GIT_TARGET
-
-# Get the GIT_TARGET
-git clone https://github.com/alejeau/$GIT_TARGET.git
+# If the $GIT_TARGET exists
+if [ -d "$GIT_TARGET" ];
+then
+# Reset the modified file and pull
+cd $GIT_TARGET \
+    && git reset src/main/resources/$CONFIG_FILE
+    && git pull
+else
+    # Clone the GIT_TARGET
+    git clone https://github.com/alejeau/$GIT_TARGET.git
+fi
 
 # Change the file
 sed -i 's/localhost:3306/'$IP_ADDRESS':'$ADDRESS_PORT'/g' $GIT_TARGET/src/main/resources/$CONFIG_FILE
 
 # Run Maven
-cd computer-database \
+cd $GIT_TARGET \
 	&& mvn clean \
 	&& mvn test
