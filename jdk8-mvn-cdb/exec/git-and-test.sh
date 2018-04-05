db6e3b9f74baca153ca00b7f35a4e29ca3e23ddd
alejeau <alejeau@excilys.com>
2018-04-05 11:59:11 +0200

    [REFACTO] Cleaned up git-and-test.sh a bit.

--- jdk8-mvn-cdb/exec/git-and-test.sh
+++ jdk8-mvn-cdb/exec/git-and-test.sh
@@ -3,7 +3,7 @@
 GIT_TARGET=computer-database
 
 # File to modify
-CONFIG_FILE=properties/db.properties
+CONFIG_FILE=src/main/resources/properties/db.properties
 
 # Changes to be made
 IP_ADDRESS=172.18.0.2
@@ -14,7 +14,7 @@ if [ -d "$GIT_TARGET" ];
 then
 # Reset the modified file and pull
 cd $GIT_TARGET \
-    && git reset src/main/resources/$CONFIG_FILE
+    && git reset $CONFIG_FILE
     && git pull
 else
     # Clone the GIT_TARGET
@@ -22,7 +22,7 @@ else
 fi
 
 # Change the file
-sed -i 's/localhost:3306/'$IP_ADDRESS':'$ADDRESS_PORT'/g' $GIT_TARGET/src/main/resources/$CONFIG_FILE
+sed -i 's/localhost:3306/'$IP_ADDRESS':'$ADDRESS_PORT'/g' $GIT_TARGET/$CONFIG_FILE
 
 # Run Maven
 cd $GIT_TARGET \
